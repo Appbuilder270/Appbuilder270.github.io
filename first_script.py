5f423c01fcce657430b9c6cd54a9ac65dc4975b4
Appbuilder270 <49277952+Appbuilder270@users.noreply.github.com>
2023-11-09 20:50:44 -0600

    Add files via upload

--- first_script.py
+++ first_script.py
@@ -1,6 +1,14 @@
 # script.py
 
+#def get_value():
+#    return "Hello, world!"
+#value = get_value()
+#print(value)
+# script.py
+
+import json
+
 def get_value():
-    return "Hello, world!"
+    return json.dumps("Hello, big world!")
 value = get_value()
 print(value)
